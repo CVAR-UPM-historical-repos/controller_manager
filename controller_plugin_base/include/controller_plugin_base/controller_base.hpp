2ac439e0d5e7b298ce914e4ef45822132c9b5914
Miguel <miferco97@gmail.com>
2022-09-26 18:57:35 +0200

    controller_based converted into interface

--- controller_plugin_base/include/controller_plugin_base/controller_base.hpp
+++ controller_plugin_base/include/controller_plugin_base/controller_base.hpp
@@ -39,90 +39,18 @@
 #ifndef CONTROLLER_BASE_HPP
 #define CONTROLLER_BASE_HPP
 
-#include <algorithm>
-#include <as2_core/control_mode_utils/control_mode_utils.hpp>
-#include <as2_core/names/topics.hpp>
-#include <as2_core/synchronous_service_client.hpp>
-#include <cstdint>
-#include <fstream>
-#include <rclcpp/logging.hpp>
-#include <rclcpp/service.hpp>
-#include <rclcpp/timer.hpp>
-#include <vector>
-
-#include <message_filters/subscriber.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <message_filters/time_synchronizer.h>
-#include "as2_core/control_mode_utils/control_mode_utils.hpp"
-#include "as2_core/names/services.hpp"
-#include "as2_core/names/topics.hpp"
-#include "as2_core/node.hpp"
-#include "as2_core/synchronous_service_client.hpp"
 #include "as2_msgs/msg/control_mode.hpp"
-#include "as2_msgs/msg/platform_info.hpp"
 #include "as2_msgs/msg/thrust.hpp"
-#include "as2_msgs/srv/list_control_modes.hpp"
-#include "as2_msgs/srv/set_control_mode.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
 #include "trajectory_msgs/msg/joint_trajectory_point.hpp"
-
-#define MATCH_ALL 0b11111111
-#define MATCH_MODE_AND_FRAME 0b11110011
-#define MATCH_MODE 0b11110000
-
-#define UNSET_MODE_MASK 0b00000000
-#define HOVER_MODE_MASK 0b00010000
 
 namespace controller_plugin_base {
 
 class ControllerBase {
-private:
-  std::vector<uint8_t> controller_available_modes_in_;
-  std::vector<uint8_t> controller_available_modes_out_;
-  std::vector<uint8_t> platform_available_modes_in_;
-
-  std::shared_ptr<message_filters::Subscriber<geometry_msgs::msg::PoseStamped>> pose_sub_;
-  std::shared_ptr<message_filters::Subscriber<geometry_msgs::msg::TwistStamped>> twist_sub_;
-  typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::msg::PoseStamped,
-                                                          geometry_msgs::msg::TwistStamped>
-      approximate_policy;
-  std::shared_ptr<message_filters::Synchronizer<approximate_policy>> synchronizer_;
-
-  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr ref_pose_sub_;
-  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr ref_twist_sub_;
-  rclcpp::Subscription<as2_msgs::msg::PlatformInfo>::SharedPtr platform_info_sub_;
-  rclcpp::Subscription<trajectory_msgs::msg::JointTrajectoryPoint>::SharedPtr ref_traj_sub_;
-
-  rclcpp::Publisher<as2_msgs::msg::Thrust>::SharedPtr thrust_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
-
-  rclcpp::Service<as2_msgs::srv::SetControlMode>::SharedPtr set_control_mode_srv_;
-  rclcpp::TimerBase::SharedPtr control_timer_;
-
-  as2_msgs::msg::PlatformInfo platform_info_;
-
-  as2::SynchronousServiceClient<as2_msgs::srv::SetControlMode>::SharedPtr set_control_mode_client_;
-  as2::SynchronousServiceClient<as2_msgs::srv::ListControlModes>::SharedPtr
-      list_control_modes_client_;
-
-  bool control_mode_established_  = false;
-  bool motion_reference_adquired_ = false;
-  bool state_adquired_            = false;
-
-  as2_msgs::msg::ControlMode input_mode_;
-  as2_msgs::msg::ControlMode output_mode_;
-
-  uint8_t prefered_output_mode_ = 0b00000000;  // by default, no output mode is prefered
-
 public:
   ControllerBase(){};
-
-  bool use_bypass_        = false;
-  bool bypass_controller_ = false;
-
-  void initialize(as2::Node* node_ptr);
 
   virtual void ownInitialize(){};
   virtual void updateState(const geometry_msgs::msg::PoseStamped& pose_msg,
@@ -140,50 +68,10 @@ public:
   virtual bool setMode(const as2_msgs::msg::ControlMode& mode_in,
                        const as2_msgs::msg::ControlMode& mode_out) = 0;
 
-  as2_msgs::msg::ControlMode getMode() { return this->input_mode_; };
-
-  void setInputControlModesAvailables(const std::vector<uint8_t>& available_modes) {
-    controller_available_modes_in_ = available_modes;
-    // sort modes in ascending order
-    std::sort(controller_available_modes_in_.begin(), controller_available_modes_in_.end());
-  };
-
-  void setOutputControlModesAvailables(const std::vector<uint8_t>& available_modes) {
-    controller_available_modes_out_ = available_modes;
-    // sort modes in ascending order
-    std::sort(controller_available_modes_out_.begin(), controller_available_modes_out_.end());
-  };
-
   virtual ~ControllerBase(){};
 
 protected:
-  as2::Node* node_ptr_;
-
-private:
-  void state_callback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr pose_msg,
-                      const geometry_msgs::msg::TwistStamped::ConstSharedPtr twist_msg);
-  void ref_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
-  void ref_twist_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
-  void ref_traj_callback(const trajectory_msgs::msg::JointTrajectoryPoint::SharedPtr msg);
-  void platform_info_callback(const as2_msgs::msg::PlatformInfo::SharedPtr msg);
-
-  geometry_msgs::msg::PoseStamped pose_;
-  geometry_msgs::msg::TwistStamped twist_;
-  geometry_msgs::msg::PoseStamped ref_pose_;
-  geometry_msgs::msg::TwistStamped ref_twist_;
-  trajectory_msgs::msg::JointTrajectoryPoint ref_traj_;
-
-  void control_timer_callback();
-  void setControlModeSrvCall(const as2_msgs::srv::SetControlMode::Request::SharedPtr request,
-                             as2_msgs::srv::SetControlMode::Response::SharedPtr response);
-  bool listPlatformAvailableControlModes();
-
-  bool tryToBypassController(const uint8_t input_mode, uint8_t& output_mode);
-  bool findSuitableOutputControlModeForPlatformInputMode(uint8_t& output_mode,
-                                                         const uint8_t input_mode);
-  bool checkSuitabilityInputMode(const uint8_t input_mode, const uint8_t output_mode);
-  void sendCommand();
-  bool setPlatformControlMode(const as2_msgs::msg::ControlMode& mode);
+  rclcpp::Node::SharedPtr node_ptr_;
 
 };  //  ControllerBase
 
